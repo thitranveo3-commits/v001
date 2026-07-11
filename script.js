98a4f87ba9cec8cb805a12da4a17266ccd45c717
thitranveo3-commits <thitranveo3@gmail.com>
2026-07-11 04:13:30 +0000

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
    ======================================== */
 
 /* -- Cache version -- */
-var CACHE_VERSION = 'v20260714';
+var CACHE_VERSION = 'v20260715';
 
 /* -- 1. Toast Notification -- */
 function showToast(message, type) {
